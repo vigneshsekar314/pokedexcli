f35ca9539bfc6b99f93311e5761edf43fe0c082c
Vignesh Sekar <vigneshsekar1996@gmail.com>
2025-09-23 23:45:14 +0530

    added help and exit command

--- main.go
+++ main.go
@@ -7,39 +7,77 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
 func main() {
 	sc := bufio.NewScanner(os.Stdin)
+	cliMapper := map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Provides help for the Pokedex",
+			callback:    commandHelp,
+		},
+	}
 	for {
 		fmt.Print("pokedex > ")
-		comm := ""
 		sc.Scan()
-		comm += sc.Text()
 		if err := sc.Err(); err != nil {
 			fmt.Fprintln(os.Stderr, "reading standard input", err)
 		}
-		commands := cleanInput(comm)
-		firstWord := strings.ToLower(commands[0])
+		commands := cleanInput(sc.Text())
 		if len(commands) > 0 {
-			fmt.Printf("Your command was: %s\n", firstWord)
+			command := commands[0]
+			cmd, ok := cliMapper[command]
+			if !ok {
+				fmt.Fprintln(os.Stderr, "Unknown command")
+				continue
+			}
+			if cmd.callback == nil {
+				fmt.Fprintln(os.Stderr, "Callback not defined")
+				continue
+			}
+			if err := cmd.callback(); err != nil {
+				fmt.Fprintln(os.Stderr, "Error in callback: ", err)
+			}
+			// switch command {
+			// case "exit":
+			// 	cmd, ok := cliMapper["exit"]
+			// 	if !ok {
+			// 		fmt.Printf("exit command not found in mapper")
+			// 	}
+			// 	if cmd.callback != nil {
+			// 		if err := cmd.callback(); err != nil {
+			// 			fmt.Fprintln(os.Stderr, "Error in callback command:", err)
+			// 		}
+			// 	}
+			// default:
+			// 	fmt.Fprintln(os.Stderr, "Unknown command")
+			// }
 		}
-
 	}
+}
 
+func commandExit() error {
+	fmt.Fprintln(os.Stdout, "Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Fprintln(os.Stdout, "Welcome to the Pokedex!\nUsage:\n\nhelp: Displays a help message\nexit: Exit the Pokedex")
+	return nil
 }
 
 func cleanInput(text string) []string {
-	result := []string{}
-	cleanText := strings.TrimSpace(text)
-	temp := ""
-	emptySpace := ' '
-	for _, letter := range cleanText {
-		if letter == emptySpace {
-			result = append(result, temp)
-			temp = ""
-		} else {
-			temp += string(letter)
-		}
-	}
-	result = append(result, temp)
-	return result
+	result := strings.ToLower(text)
+	return strings.Fields(result)
 }
